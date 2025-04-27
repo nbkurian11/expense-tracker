726d50f103e100cd702ec7e7450c80da45a97035
Noel Kurian <70000001+nbkurian11@users.noreply.github.com>
2025-04-27 15:12:25 -0400

    Update income.py

--- income.py
+++ income.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 class Income:
     def __init__(self, amount):
         self.amount = amount
-=======
-class Income:
-    def __init__(self, amount):
-        self.amount = amount
->>>>>>> 29d8e2086597f5b4e24f69bf6f8c30c5093bffd4
+
