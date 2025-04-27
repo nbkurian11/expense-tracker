a3e2810cb1ec66281c42b7000b0b2cf430ed5386
Noel Kurian <70000001+nbkurian11@users.noreply.github.com>
2025-04-27 15:12:48 -0400

    Update expense.py

--- expense.py
+++ expense.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 class Expense:
     def __init__(self, expense_type, amount):
         self.expense_type = expense_type
         self.amount = amount
-=======
 
-class Expense:
-    def __init__(self, type, amount):
-        self.type = type
-        self.amount = amount
->>>>>>> 29d8e2086597f5b4e24f69bf6f8c30c5093bffd4
