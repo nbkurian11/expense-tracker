e6b434e60dc7d941f2f443595fc0234b1e85e0ca
Noel Kurian <nbkurian11@gmail.com>
2025-04-27 15:00:21 -0400

    Added basic template of the project

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<p>Expense Tracker</p>"
+
+@app.route("/add-income")
+def income():
+    return "<p>Add Income page</p>"
+
+@app.route("/add-expenses")
+def expenses():
+    return "<p>Add expenses page</p>"
+
+@app.route("/view-transactions")
+def transactions():
+    return "<p>View Transactions</p>"
